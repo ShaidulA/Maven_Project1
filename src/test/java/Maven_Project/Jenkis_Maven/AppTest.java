94e6fda86c0224b00e841b3f9c82eb8ee7e12b48
shidul <shaidulazam@gmail.com>
2024-02-12 12:29:13 -0500

    Simple maven Junit test

---
+++ src/test/java/Maven_Project/Jenkis_Maven/AppTest.java
@@ -0,0 +1,23 @@
+package Maven_Project.Jenkis_Maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void test1()
+    {
+    	System.out.println("Hello Maven");  
+    	}
+    @Test
+    public void test2()
+    {
+    	System.out.println("This is Shaidul Azam");
+    }
+    
+}
